955dc3fdb400785a78d6bd6cdb49abb71ce6d8ce
nn376 <46791642+nn376@users.noreply.github.com>
2021-03-14 22:23:20 -0400

    commit

---
+++ js/hugrid-functions.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+        pageUnits = 'px';
+        colUnits = 'px';
+        pagewidth = 940;
+        columns = 12;
+        columnwidth = 60;
+        gutterwidth = 20;
+        pagetopmargin = 0;
+        rowheight = 20;
+        gridonload = 'off';
+        makehugrid();
+        setgridonload();
+    });
---
+++ js/script.js
@@ -0,0 +1,82 @@
+$.scrollIt({
+  upKey: 38,             // key code to navigate to the next section
+  downKey: 40,           // key code to navigate to the previous section
+  easing: 'linear',      // the easing function for animation
+  scrollTime: 600,       // how long (in ms) the animation takes
+  activeClass: 'active', // class given to the active nav element
+  onPageChange: null,    // function(pageIndex) that is called when page is changed
+  topOffset: 0           // offste (in px) for fixed top navigation
+});
+var myElement = document.querySelector("header");
+var headroom = new Headroom(myElement);
+headroom.init();
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
+$(document).ready(function() {
+  //To Swithch directions up/down and left/right just place a "-" in front of the top/left attribute
+  //Full Caption Sliding (Hiddent to Visible)
+  $('.boxgrid.captionfull').hover(function() {
+   $(".cover", this).stop().animate({
+     top:'160px'
+   }, {
+     queue: false,
+     duration: 160
+   });
+  }, function() {
+    $(".cover", this).stop().animate({
+      top: '260px'
+    }, {
+      queue:false,
+      duration:160
+    });
+  });
+});
